3e2cc305764d125be52b44ae91229a0455473260
Yinzhi Huang <yinzhi93@gmail.com>
2020-10-07 22:20:35 +1300

    Add customer and order controllers

--- OCL_Apis/Program.cs
+++ OCL_Apis/Program.cs
@@ -21,17 +21,17 @@ namespace OCL_Apis
 
             using (var scope = host.Services.CreateScope())
             {
-                var services = scope.ServiceProvider;
+                //var services = scope.ServiceProvider;
 
-                try
-                {
-                    SeedData.Initialize(services);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
+                //try
+                //{
+                //    SeedData.Initialize(services);
+                //}
+                //catch (Exception ex)
+                //{
+                //    var logger = services.GetRequiredService<ILogger<Program>>();
+                //    logger.LogError(ex, "An error occurred seeding the DB.");
+                //}
             }
 
             host.Run();
